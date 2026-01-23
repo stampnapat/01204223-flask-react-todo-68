0d2143ddce5603496197f7f81a16a34b244a3786
stampnapat <stampnapatt@gmail.com>
2026-01-23 15:14:44 +0700

    frist_frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,39 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+class Base(DeclarativeBase):
+  pass
+db = SQLAlchemy(app, model_class=Base)
+
+class TodoItem(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    done: Mapped[bool] = mapped_column(default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "done": self.done
+        }
+with app.app_context():
+    db.create_all()
+    INITIAL_TODOS = [
+    TodoItem(title='Learn Flask'),
+    TodoItem(title='Build a Flask App'),
+]
+
+with app.app_context():
+    if TodoItem.query.count() == 0:
+         for item in INITIAL_TODOS:
+             db.session.add(item)
+         db.session.commit()
 
 todo_list = [
     { "id": 1,
@@ -15,30 +46,21 @@ todo_list = [
 
 @app.route('/api/todos/', methods=['GET'])
 def get_todos():
-    return jsonify(todo_list)
+    todos = TodoItem.query.all()
+    return jsonify([todo.to_dict() for todo in todos])
 
 def new_todo(data):
-    if len(todo_list) == 0:
-        id = 1
-    else:
-        id = 1 + max([todo['id'] for todo in todo_list])
-
-    if 'title' not in data:
-        return None
-    
-    return {
-        "id": id,
-        "title": data['title'],
-        "done": getattr(data, 'done', False),
-    }
+    return TodoItem(title=data['title'], 
+           done=data.get('done', False))
 
 @app.route('/api/todos/', methods=['POST'])
 def add_todo():
     data = request.get_json()
     todo = new_todo(data)
     if todo:
-        todo_list.append(todo)
-        return jsonify(todo)
+        db.session.add(todo)                   
+        db.session.commit()                        
+        return jsonify(todo.to_dict())    
     else:
         # return http response code 400 for bad requests
         return (jsonify({'error': 'Invalid todo data'}), 400)  
@@ -46,19 +68,14 @@ def add_todo():
 
 @app.route('/api/todos/<int:id>/toggle/', methods=['PATCH'])
 def toggle_todo(id):
-    todos = [todo for todo in todo_list if todo['id'] == id]
-    if not todos:
-        return (jsonify({'error': 'Todo not found'}), 404)
-    todo = todos[0]
-    todo['done'] = not todo['done']
-    return jsonify(todo)
-
+    todo = TodoItem.query.get_or_404(id)
+    todo.done = not todo.done
+    db.session.commit()
+    return jsonify(todo.to_dict())
 
 @app.route('/api/todos/<int:id>/', methods=['DELETE'])
 def delete_todo(id):
-    global todo_list
-    todos = [todo for todo in todo_list if todo['id'] == id]
-    if not todos:
-        return (jsonify({'error': 'Todo not found'}), 404)
-    todo_list = [todo for todo in todo_list if todo['id'] != id]
+    todo = TodoItem.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
     return jsonify({'message': 'Todo deleted successfully'})
