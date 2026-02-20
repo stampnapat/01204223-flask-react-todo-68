4bc81ba1cc025529f9a53eab445d959864a8e8e3
stampnapat <stampnapatt@gmail.com>
2026-02-20 14:46:42 +0700

    route

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
+import click
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-from models import TodoItem, Comment, db
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager
 
+from models import TodoItem, Comment, User, db
 
 app = Flask(__name__)
 # CORS(app, resources={r"/api/*": {"origins": [
@@ -16,6 +19,9 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todos.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+jwt = JWTManager(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -91,3 +97,39 @@ def add_comment(todo_id):
     db.session.commit()
 
     return jsonify(comment.to_dict())
+
+
+@app.cli.command("create-user")
+@click.argument("username")
+@click.argument("full_name")
+@click.argument("password")
+def create_user(username, full_name, password):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        click.echo("User already exists.")
+        return
+
+    user = User(username=username, full_name=full_name)
+    user.set_password(password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    click.echo(f"User {username} created successfully.")
+
+@app.route('/api/login/', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({'error': 'Username and password are required'}), 400
+
+    user = User.query.filter_by(username=data['username']).first()
+    if not user or not user.check_password(data['password']):
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=user.username)
+    return jsonify(access_token=access_token
+
+
+app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+jwt = JWTManager(app)
